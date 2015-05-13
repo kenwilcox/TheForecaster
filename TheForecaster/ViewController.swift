471bbb059eadf1b36202fb3aa72ac33546e655de
Ken Wilcox <wilcoxk@gmail.com>
2015-05-12 22:13:59 -0600

    main UI placement

--- TheForecaster/ViewController.swift
+++ TheForecaster/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var weatherImageView: UIImageView!
+  @IBOutlet weak var weatherConditionsLabel: UILabel!
+  @IBOutlet weak var updateDateLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBAction func refreshWeatherButtonPressed(sender: UIButton) {
+  }
+  
 }
 
