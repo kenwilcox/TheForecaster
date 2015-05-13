c1bb28b0b0fc5732608640cd198a1e74c2c05185
Ken Wilcox <wilcoxk@gmail.com>
2015-05-12 22:58:58 -0600

    Getting Location stuff going...

---
+++ TheForecaster/LocationController.swift
@@ -0,0 +1,61 @@
+//
+//  LocationController.swift
+//  TheForecaster
+//
+//  Created by Kenneth Wilcox on 5/12/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationController: NSObject {
+  
+  var locationManager:CLLocationManager!
+  class var locationDidUpdate: String { return "LOCATION_DID_UPDATE" }
+  
+  override init() {
+    
+    super.init()
+    
+    self.locationManager = CLLocationManager()
+    self.locationManager.delegate = self
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    self.locationManager.requestAlwaysAuthorization()
+    self.locationManager.distanceFilter = 100.0
+    self.locationManager.startUpdatingLocation()
+  }
+  
+}
+
+// MARK: - CLLocationManagerDelegate
+extension LocationController: CLLocationManagerDelegate {
+  
+  func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    
+    let location = locations.last as! CLLocation
+    println("didUpdateLocations: \(location)")
+    
+    let geocoder = CLGeocoder()
+    geocoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+      if error != nil {
+        println("Error reverse geocoding: \(error)")
+      } else {
+        let placemark = placemarks.last as! CLPlacemark
+        
+        let locationInfo = [
+          "latitude": placemark.location.coordinate.latitude,
+          "longitude": placemark.location.coordinate.longitude,
+          "city": placemark.locality,
+          "state": placemark.administrativeArea,
+          "country": placemark.country,
+          "timestamp": location.timestamp
+          ] as NSDictionary
+        
+        NSNotificationCenter.defaultCenter().postNotificationName(LocationController.locationDidUpdate, object: nil, userInfo: locationInfo as [NSObject : AnyObject])
+        
+      }
+    })
+
+  }
+}
--- TheForecaster/ViewController.swift
+++ TheForecaster/ViewController.swift
@@ -19,11 +19,16 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "locationDidUpdate:", name: LocationController.locationDidUpdate, object: nil)
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func locationDidUpdate(notification: NSNotification) {
+    println("locationAvaliable")
   }
   
   @IBAction func refreshWeatherButtonPressed(sender: UIButton) {
