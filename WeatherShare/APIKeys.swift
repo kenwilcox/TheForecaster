17e7e59580688b34464744b89fb0865532ca6545
Ken Wilcox <wilcoxk@gmail.com>
2015-05-25 17:34:45 -0600

    updated gitignore

---
+++ WeatherShare/APIKeys.swift
@@ -0,0 +1,9 @@
+//
+//  APIKeys.swift
+//  TheForecaster
+//
+//  Created by Kenneth Wilcox on 5/25/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherShare/ForecastNetwork.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastNetwork.swift
+//  TheForecaster
+//
+//  Created by Kenneth Wilcox on 5/25/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class ForecastNetwork: NSObject {
+   
+}
