8965512183a8f62b8ee6abab8154f6507bf0eac1
Ken Wilcox <wilcoxk@gmail.com>
2015-05-31 20:23:07 -0600

    connected interface elements

--- TheForecaster WatchKit Extension/InterfaceController.swift
+++ TheForecaster WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,36 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
+  
+  @IBOutlet weak var temperatureLabel: WKInterfaceLabel!
+  @IBOutlet weak var weatherIconImage: WKInterfaceImage!
+  @IBOutlet weak var conditionsLabel: WKInterfaceLabel!
+  @IBOutlet weak var locationLabel: WKInterfaceLabel!
+  @IBOutlet weak var map: WKInterfaceMap!
+  @IBOutlet weak var lastUpdatedLabel: WKInterfaceLabel!
+  
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  @IBAction func hourlyWeatherButtonPressed() {
+    
+  }
+  
+  @IBAction func refreshButtonPressed() {
+    
+  }
+  
 }
